1a90ff1a36248c600578fccb38b701925945f9f3
RavenVR <100008567+RavenVR@users.noreply.github.com>
2022-10-05 16:56:02 +1000

    Add files via upload

---
+++ crappynumberfinder.py
@@ -0,0 +1,31 @@
+import random as r
+from time import *
+
+
+w = False
+a = int(input("Please input any number "))
+w = False
+if a < 0:
+    w = True
+while w == True:
+    if a < 0:
+        a = int(input("Please input any number "))
+    elif a > 0:
+        w = False
+        break
+
+print("guessing the number, it will aprox take: ", a/100, "seconds.")
+guess = True
+num = 0
+while guess == True:
+    sleep(0.01)
+    num += 1
+    num = round(num)
+    print("\r", num, end='')
+    if num == a:
+        guess = False
+        break
+
+print("\rthe number was: ", num)
+
+sleep(5)
