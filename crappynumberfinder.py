68356deeaf07deda824930a0b697f3e7f8964992
RavenVR <100008567+RavenVR@users.noreply.github.com>
2022-10-07 09:09:19 +1000

    Update crappynumberfinder.py

--- crappynumberfinder.py
+++ crappynumberfinder.py
@@ -1,12 +1,17 @@
 from time import *
 import random as r
 a1 = open(r"cnf-log.txt", "a")
-
+a = 0
+guess = False
+num = 0
+log = ''
+log1=''
 def convertTuple(tup):
     log1 = ''.join(tup)
     return log1
-mode = input("wich mode do you want? (random[r] or normal[n])? ")
+mode = input("which mode do you want? (random[r] or normal[n])? ")
 mode = mode.lower()
+h=False
 if mode == "n":
 
     while True:
@@ -15,19 +20,26 @@ if mode == "n":
             if a < 0:
                 a = int(input("Please enter a number: "))
             elif a > 0:
+                h = True
                 break
         except ValueError:
             print ("Oops!  That was no valid number.  Try again...")
-            sleep(2)
+            sleep(1)
     print("guessing the number, it will aprox take: ", a/10000, "seconds.")
+    
 elif mode == "r":
     a = r.randint(1,600000)
-time = str(a/10000)
-number = str(a)
-log1 = "The number: ",number," took: ",time," seconds."
-log = convertTuple(log1)
-guess = True
-num = 0
+    h = True
+else:
+    print('please try again!')
+    sleep(1)
+while h == True:
+    time = str(a/100000)
+    number = str(a)
+    log1 = "The number: ",number," took: ",time," seconds."
+    log = convertTuple(log1)
+    guess = True
+    h = False
 while guess == True:
     
     num += 1
@@ -36,6 +48,7 @@ while guess == True:
     if num == a:
         guess = False
         break
+    
 
 print("\rthe number was: ", num)
 a1.write(log + "\n")
