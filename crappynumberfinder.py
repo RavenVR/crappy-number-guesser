7d4c80661d852d6e2beba61adfa97abb845c48ac
RavenVR <100008567+RavenVR@users.noreply.github.com>
2022-10-06 15:00:01 +1000

    added random mode

--- crappynumberfinder.py
+++ crappynumberfinder.py
@@ -1,25 +1,27 @@
-import random as r
 from time import *
+import random as r
+a1 = open(r"cnf-log.txt", "a")
 
 def convertTuple(tup):
     log1 = ''.join(tup)
     return log1
+mode = input("wich mode do you want? (random[r] or normal[n])? ")
+mode = mode.lower()
+if mode == "n":
 
-a1 = open(r"cnf-log.txt", "a")
-
-while True:
-    try:
-        a = int(input("Please enter a number: "))
-        if a < 0:
+    while True:
+        try:
             a = int(input("Please enter a number: "))
-        elif a > 0:
-            break
-    except ValueError:
-        print ("Oops!  That was no valid number.  Try again...")
-        sleep(2)
-    
-
-print("guessing the number, it will aprox take: ", a/10000, "seconds.")
+            if a < 0:
+                a = int(input("Please enter a number: "))
+            elif a > 0:
+                break
+        except ValueError:
+            print ("Oops!  That was no valid number.  Try again...")
+            sleep(2)
+    print("guessing the number, it will aprox take: ", a/10000, "seconds.")
+elif mode == "r":
+    a = r.randint(1,600000)
 time = str(a/10000)
 number = str(a)
 log1 = "The number: ",number," took: ",time," seconds."
